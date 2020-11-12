829b1c041724f994b74d5eefbe77f163f76a2b82
Sonia Cardinale <sonia.cardinale@avanade.com>
2020-11-12 15:03:55 +0100

    Prova Commit

--- Academy.DBTest/Form1.Designer.cs
+++ Academy.DBTest/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_Clienti = new System.Windows.Forms.Button();
+            this.lst_Clienti = new System.Windows.Forms.ListBox();
+            this.lst_ContiCorrenti = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btn_Clienti
+            // 
+            this.btn_Clienti.Location = new System.Drawing.Point(41, 35);
+            this.btn_Clienti.Name = "btn_Clienti";
+            this.btn_Clienti.Size = new System.Drawing.Size(151, 43);
+            this.btn_Clienti.TabIndex = 0;
+            this.btn_Clienti.Text = "Clienti";
+            this.btn_Clienti.UseVisualStyleBackColor = true;
+            // 
+            // lst_Clienti
+            // 
+            this.lst_Clienti.FormattingEnabled = true;
+            this.lst_Clienti.ItemHeight = 20;
+            this.lst_Clienti.Location = new System.Drawing.Point(41, 122);
+            this.lst_Clienti.Name = "lst_Clienti";
+            this.lst_Clienti.Size = new System.Drawing.Size(151, 124);
+            this.lst_Clienti.TabIndex = 1;
+            this.lst_Clienti.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // lst_ContiCorrenti
+            // 
+            this.lst_ContiCorrenti.FormattingEnabled = true;
+            this.lst_ContiCorrenti.ItemHeight = 20;
+            this.lst_ContiCorrenti.Location = new System.Drawing.Point(274, 122);
+            this.lst_ContiCorrenti.Name = "lst_ContiCorrenti";
+            this.lst_ContiCorrenti.Size = new System.Drawing.Size(173, 124);
+            this.lst_ContiCorrenti.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lst_ContiCorrenti);
+            this.Controls.Add(this.lst_Clienti);
+            this.Controls.Add(this.btn_Clienti);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Clienti;
+        private System.Windows.Forms.ListBox lst_Clienti;
+        private System.Windows.Forms.ListBox lst_ContiCorrenti;
     }
 }
 
--- Academy.DBTest/Form1.cs
+++ Academy.DBTest/Form1.cs
@@ -16,5 +16,10 @@ namespace Academy.DBTest
         {
             InitializeComponent();
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
